3a22132b7bf0411c10b9806fc205a7ca6558301f
Luke Santoro <luke@betterment.com>
2017-06-14 15:02:37 -0400

    add restaurant routes and form and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome', to: 'welcome#index'
   get 'home', to: 'home#index'
 
+  resources :restaurants
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
